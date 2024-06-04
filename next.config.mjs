dfc160e4f6225315da5215a98f6dce6158cb2f92
David Morris <david.morris@nefino.de>
2024-06-04 11:07:36 +0200

    initial commit, should show build differences

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import CopyPlugin from 'copy-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config, { isServer }) {
+        if (isServer) {
+            console.log('running webpack config for server');
+            config.plugins.push(new CopyPlugin({
+                patterns: [
+                    { from: "src/app/binaryFile.copyme", to: "[path][name][ext]" },
+                ]
+            }))
+        }
+        return config
+    }
+};
 
 export default nextConfig;
